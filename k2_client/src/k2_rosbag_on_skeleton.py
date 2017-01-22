b330a665dcf36359169f048ff642a3442b8c102d
Claudio Coppola <claudiocoppola90@gmail.com>
2017-01-22 00:30:51 +0000

    rosbag recording completed

--- k2_client/src/k2_rosbag_on_skeleton.py
+++ k2_client/src/k2_rosbag_on_skeleton.py
@@ -1,54 +1,62 @@
+#!/usr/bin/env python   
 import rospy
 import time
 import os
-import signal
+import stat
+import signal,psutil
 import sys
 from datetime import datetime
 from rosbag import rosbag_main
 from multiprocessing import Process,RLock
 from k2_client.msg import Body,BodyArray
 
-
 class SkeletonRosbag(object):
 
 
     def __init__(self):
-        self.out_folder='/home/ccoppola/recording_Test/'
-        os.makedirs(self.out_folder)
         self.sub = rospy.Subscriber('/head/kinect2/k2_bodies/bodies',BodyArray,self.process_message)
         self.recording_status=False
         self.writing_process=None
         self.last_message_time=[]
         self.timer=None
         self.lock=RLock()
-        #  b = rosbag.Bag()
-        
-
-
 
     def start_recording(self):
-        #  self.last_message_time=
+
         n=datetime.now()
         nowstring =\
         str(n.year).zfill(4)+str(n.month).zfill(2)+str(n.day).zfill(2)+str(n.hour).zfill(2)+str(n.minute).zfill(2)+str(n.second).zfill(2)
-        #  rosbag_main.record_cmd
+        rosbag_main.record_cmd
         self.writing_process = Process(target=rosbag_main.record_cmd,args = \
-                                       [['/head/kinect2/k2_rgb/image/compressed\
-                                         /head/kinect2/k2_depth/image\
-                                         /head/kinect2/k2_depth/camera_info\
-                                         /head/kinect2/k2_bodies/bodies\
-                                         /head/kinect2/k2_audio/audio -O '+self.out_folder,'/recording_'+nowstring+'.bag']])
+                                       [['/head/kinect2/k2_rgb/image/compressed',\
+                                         '/head/kinect2/k2_depth/image',\
+                                         '/head/kinect2/k2_depth/camera_info',\
+                                         '/head/kinect2/k2_bodies/bodies',\
+                                         '/head/kinect2/k2_audio/audio',\
+                                         '-Orecording_'+nowstring]])
+        self.writing_process.start()
+        print "recording started at time: "+nowstring
 
     
-    def stop_recording(self):
+    def stop_recording(self,event):
         with self.lock:
-            os.kill(int(self.writing_process.pid),signal.SIGINT)
+            try:
+
+                parent=psutil.Process(self.writing_process.pid)
+                children= parent.get_children(recursive=True)
+                for child_process in children:
+                    child_process.send_signal(signal.SIGINT)
+            except psutil.NoSuchProcess:
+                pass
+            #  os.kill(int(self.writing_process.pid),signal.SIGINT)
             self.timer=None
+        print "recording stopped"
 
 
     def process_message(self,data):
-        #body message arrived
+        #  print "body message arrived"
         with self.lock:
+            #  print self.timer
             if self.timer == None:
                 self.start_recording()
             else:
@@ -60,27 +68,16 @@ class SkeletonRosbag(object):
 
 
 
-
-
-
-
-
-
-
-
-
-
 def main():
-     writer=SkeletonRosbag()
-     rospy.init_node('skeleton_recorder', anonymous=True)
+    writer=SkeletonRosbag()
+    rospy.init_node('skeleton_recorder', anonymous=True)
 
 
-     try:
-         rospy.spin()
+    try:
+        rospy.spin()
               
-     except KeyboardInterrupt:
-        print 'Shutting down'   
-
+    except KeyboardInterrupt:
+       print 'Shutting down'   
 
 
 
